cb92ef482e6096df7bf7f1edae06f7d34b79915e
benni <albi.benni8@gmail.com>
2022-06-23 21:29:02 +0200

    userprofiles and useffect

--- src/axios/UserProfiles.js
+++ src/axios/UserProfiles.js
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const UserProfiles = () => {
-  const [userInfo, setUserInfo] = useState([]);
+  const [userProfiles, setUserProfiles] = useState([]);
 
   const fetchUserProfiles = () => {
     axios.get("http://localhost:8080/user-profile").then((res) => {
       console.log(res);
-      setUserInfo(res);
+      setUserProfiles(res.data);
     });
   };
 
@@ -15,7 +15,14 @@ const UserProfiles = () => {
     fetchUserProfiles();
   }, []);
 
-  return <div></div>;
+  return userProfiles.map((userProfiles, index) => {
+    return (
+      <div>
+        <h1>{userProfiles.username}</h1>
+        <p>{userProfiles.userProfileId}</p>
+      </div>
+    );
+  });
 };
 
 export default UserProfiles;
